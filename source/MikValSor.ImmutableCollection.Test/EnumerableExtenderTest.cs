20395610ee0c997ed15895e5ce1eff6371de9109
Mikkel Valentin Sørensen <mvs@codehouse.com>
2018-05-03 15:39:51 +0200

    Added EnumerableExtender like: IEnumearble<T>.ToImmutable()

---
+++ source/MikValSor.ImmutableCollection.Test/EnumerableExtenderTest.cs
@@ -0,0 +1,49 @@
+﻿using MikValSor.Immutable;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace MikValSor.ImmutableCollection.Test
+{
+	[TestFixture]
+	public class EnumerableExtenderTest
+	{
+		[Test]
+		public void ToImmutable_Array()
+		{
+			//Arrange
+			var source = new char[] { 'A' };
+			var immutableCollection = EnumerableExtender.ToImmutable(source);
+
+			//Act
+			source[0] = 'B';
+
+			//Assert
+			Assert.AreEqual('A', immutableCollection[0]);
+		}
+		[Test]
+		public void ToImmutable_List()
+		{
+			//Arrange
+			var source = new List<char> { 'A' };
+			var immutableCollection = EnumerableExtender.ToImmutable(source);
+
+			//Act
+			source[0] = 'B';
+
+			//Assert
+			Assert.AreEqual('A', immutableCollection[0]);
+		}
+		[Test]
+		public void ToImmutable_ImmutableCollection()
+		{
+			//Arrange
+			var source = new ImmutableCollection<char>(new char[] { 'A' });
+
+			//Act
+			var immutableCollection = EnumerableExtender.ToImmutable(source);
+
+			//Assert
+			Assert.AreEqual('A', immutableCollection[0]);
+		}
+	}
+}
---
+++ source/MikValSor.ImmutableCollection/EnumerableExtender.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MikValSor.Immutable
+{
+	/// <summary>
+	///		Extender class for bringing immutable extensions to Enumerable classes
+	/// </summary>
+	public static class EnumerableExtender
+	{
+		/// <summary>
+		///		Function for creating a Immutable representation of the source enumerable.
+		/// </summary>
+		/// <typeparam name="TSource">
+		///		Type of the IEnumerable
+		/// </typeparam>
+		/// <param name="source">
+		///		Enumerable source to create immutable collection.
+		/// </param>
+		/// <returns></returns>
+		public static ImmutableCollection<TSource> ToImmutable<TSource>(this IEnumerable<TSource> source)
+		{
+			var result = source as ImmutableCollection<TSource>;
+			if (result != null) return result;
+			var list = source as IList<TSource>;
+			if (list == null)
+			{
+				list = source.ToList();
+			}
+			return new ImmutableCollection<TSource>(list);
+		}
+	}
+}
